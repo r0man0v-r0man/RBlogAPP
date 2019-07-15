3ee15842e97692b61038763a1f511a1843dfbfd6
Roman <romanovmailbox@mail.ru>
2019-07-15 23:31:36 +0300

    local user registration, again anderstand how it work

--- RBlog.WEB/Models/Auth/RegisterViewModel.cs
+++ RBlog.WEB/Models/Auth/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace RBlog.WEB.Models.Auth
 {
     public class RegisterViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
